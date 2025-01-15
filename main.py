6c0a674091198523993e8fbeb3431ab99b9484c5
KadolValery <71660325+KadolValery@users.noreply.github.com>
2025-01-15 14:17:13 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+#task 1
+print("this is task 1:")
+print(17/2*3+2)
+print(2+17/2*3)
+print(19%4+15/2*3)
+print(15+6-10*4)
+print(17/2%2*3**3,"\n")
+
+
+#task 2
+print("this is task 2:")
+print(17/2*(3+2))
+print((2+17)/2*3)
+print(19%(4+15)/(2*3))
+print((15+(6-10))*4)
+print((17/2)%(2*3)**3,"\n")
+
+#task 3
+print("this is task 3:")
+y = 11
+x = 1.5
+z = 3
+print("Рублей осталось: ", y-(x*z),"\n")
+
+#task 4
+print("this is task 4:")
+anna = 2
+pol = 5
+print("Anna : " +str(anna), "\n"
+      "Pol : " +str(pol), "\n")
+
+#task 5
+print("this is task 5:")
+days = 5
+print(days, " Суток = ",  days*24, " часов = ", days * 60 *24, " минут = ", days * 60 * 24 * 60, " секунд", "\n" )
+
+#task 6
+print("this is task 6:")
+
+d = 182
+w = 7
+print(d//w)
+
+
+
